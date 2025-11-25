ca59d36afafbb20e1d7fd5cd6d81e5d656fecd21
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2025-11-25 16:33:51 -0500

    Transition to mujoco simulator

---
+++ src/mujoco_env.py
@@ -0,0 +1,26 @@
+from gymnasium.envs.mujoco.humanoid_v5 import HumanoidEnv
+
+
+class PoppyEnv(HumanoidEnv):
+    def __init__(self, model_path: str, **kwargs):
+        super().__init__(model_path, **kwargs)
+
+    def _get_rew(self, x_velocity: float, action):
+        forward_reward = self._forward_reward_weight * x_velocity
+        healthy_reward = self.healthy_reward
+        rewards = forward_reward + healthy_reward
+
+        ctrl_cost = self.control_cost(action)
+        contact_cost = self.contact_cost
+        costs = ctrl_cost + contact_cost
+
+        reward = rewards - costs
+
+        reward_info = {
+            "reward_survive": healthy_reward,
+            "reward_forward": forward_reward,
+            "reward_ctrl": -ctrl_cost,
+            "reward_contact": -contact_cost,
+        }
+
+        return reward, reward_info
